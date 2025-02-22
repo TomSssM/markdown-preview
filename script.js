127751842e6b6e81ef662c366b692bc853e3789e
TomSssM <ilyashome3@gmail.com>
2025-02-22 23:20:28 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const Themes = {
+    Light: 'vscode-light',
+    Dark: 'vscode-dark'
+};
+
+class Theme {
+    static THEME_KEY = 'theme';
+
+    static getTheme() {
+        return window.localStorage.getItem(this.THEME_KEY) || Themes.Light;
+    }
+
+    static setTheme(theme) {
+        Object.values(Themes).forEach((theme) => {
+            document.body.classList.remove(theme);
+        });
+
+        document.body.classList.add(theme);
+        window.localStorage.setItem(this.THEME_KEY, theme);
+    }
+
+    static onThemeChange = () => {
+        const theme = document.body.classList.contains(Themes.Dark) ? Themes.Light : Themes.Dark;
+
+        this.setTheme(theme);
+    }
+
+    static initTheme = () => {
+        this.setTheme(this.getTheme());
+    }
+}
+
+document.getElementById('switch').addEventListener('click', Theme.onThemeChange);
+
+Theme.initTheme();
